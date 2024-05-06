182fa45c9403aea14d03e58727d29b7ccfade422
AImaltsev <boomer47.am@gmail.com>
2024-05-06 21:56:25 +0400

    Add and del Task

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<Tasklist />
+</template>
+
+<script>
+import Tasklist from './components/Tasklist.vue';
+
+export default{
+  components: {
+    Tasklist,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Tasklist.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>Task list</h1>
+  <p>Please enter you task</p>
+  <input type="text" v-model="newTask">
+  <button @click="AddTask">add task</button>
+  <div>
+  <ul>
+    <li v-for="(task, index) in taskArray" :key="index">{{ task }} <button @click="deleteTask(index)">Delete</button></li>
+  </ul>
+  </div>
+</template>
+  
+<script>
+export default{
+  data() {
+    return {
+      newTask: '', 
+      taskArray: [],
+    };
+  },
+  methods: {
+    AddTask() {
+      if (this.newTask.trim() !== '') {
+        this.taskArray.push(this.newTask);
+        this.newTask = ''
+      }
+    },
+    deleteTask(index) {
+      this.taskArray.splice(index,1)
+    },
+  }
+}
+</script>
+  
+<style scoped>
+  
+</style>
+  
