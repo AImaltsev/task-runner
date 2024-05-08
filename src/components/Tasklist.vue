fb817fe651b8f171b5cc30b470048225e922a145
AImaltsev <boomer47.am@gmail.com>
2024-05-08 16:44:43 +0400

    add edit, del and save task

--- src/components/Tasklist.vue
+++ src/components/Tasklist.vue
@@ -3,9 +3,16 @@
   <p>Please enter you task</p>
   <input type="text" v-model="newTask">
   <button @click="AddTask">add task</button>
+  
   <div>
   <ul>
-    <li v-for="(task, index) in taskArray" :key="index">{{ task }} <button @click="deleteTask(index)">Delete</button></li>
+    <li v-for="(task, index) in taskArray" :key="index">
+      <span>{{ task }}</span>
+      <button @click="editTask(index)">edit</button>
+      <button @click="deleteTask(index)">Delete</button>
+      <input type="text" v-if="index === editIndex" v-model="textEditTask">
+      <button v-if="index === editIndex" @click="saveTask(index)">Save</button>
+    </li>
   </ul>
   </div>
 </template>
@@ -16,6 +23,8 @@ export default{
     return {
       newTask: '', 
       taskArray: [],
+      editIndex: -1,
+      textEditTask: '',
     };
   },
   methods: {
@@ -25,8 +34,19 @@ export default{
         this.newTask = ''
       }
     },
+    editTask(index){
+      this.editIndex = index
+      this.textEditTask = this.taskArray[index]
+    },
     deleteTask(index) {
       this.taskArray.splice(index,1)
+    },
+    saveTask(index) {
+      if (this.textEditTask.trim() !== '') {
+        this.taskArray[index] = this.textEditTask;
+        this.editIndex = -1;
+        this.textEditTask = ''; 
+  }
     },
   }
 }
